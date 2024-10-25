e52b814300f7d40da81f7aef79a382e1cbe82ba8
valtterivuo <82218422+valtterivuo@users.noreply.github.com>
2024-10-25 20:18:24 +0300

    Added game over UI elements

--- src/entities/overlays/StatusBar.js
+++ src/entities/overlays/StatusBar.js
@@ -7,6 +7,7 @@ export class StatusBar {
 
         this.time = 99;
         this.timeTimer = 0;
+        this.gameOver = false; // Flag to track if the game is over
 
         this.healthBars = [{
             timer: 0,
@@ -29,7 +30,14 @@ export class StatusBar {
             ['time-8', [860, 409, 38, 44]],
             ['time-9', [900, 409, 39, 44]],
             ['time-0', [941, 409, 38, 44]],
-            
+
+            ['k', [585, 285, 38, 45]],
+            ['o', [757, 285, 38, 45]],
+
+            ['t', [960, 286, 37, 45]],
+            ['i', [911, 231, 39, 44]],
+            ['m', [671, 285, 40, 45]],
+            ['e', [749, 231, 39, 44]],         
         ]);
     }
 
@@ -60,21 +68,36 @@ export class StatusBar {
     }
 
     checkGameEnd() {
-        // Check if the timer or any fighter's hit points have reached zero
         const timerExpired = this.time <= 0;
         const fighterHealthDepleted = this.healthBars.some(bar => bar.hitPoints <= 0);
-    
-        // If either condition is true, end the game
+
         if (timerExpired || fighterHealthDepleted) {
-            // Add logic to end the game here, such as displaying game over screen or resetting the game
-            console.log("Game Over!");
+            this.gameOver = true; // Set the game over flag
         }
     }
-    
+
+    onEnd(context) {
+        // Display KO if a fighter's health is zero
+        if (this.healthBars.some(bar => bar.hitPoints <= 0)) {
+            this.drawFrame(context, 'k', 910, 500);
+            this.drawFrame(context, 'o', 955, 500);
+        }
+
+        // Display "TIME" if timer has expired
+        if (this.time <= 0) {
+            this.drawFrame(context, 't', 865, 500);
+            this.drawFrame(context, 'i', 910, 500);
+            this.drawFrame(context, 'm', 955, 500);
+            this.drawFrame(context, 'e', 1000, 500);
+        }
+    }
+
     update(time) {
-        this.updateTime(time);
-        this.updateHealthBars(time);
-        this.checkGameEnd();
+        if (!this.gameOver) { // Only update if the game is not over
+            this.updateTime(time);
+            this.updateHealthBars(time);
+            this.checkGameEnd();
+        }
     }
 
     drawHealthBars(context) {
@@ -92,7 +115,6 @@ export class StatusBar {
         context.fillRect(
             1038 + Math.floor(this.healthBars[1].hitPoints), 71,
             HEALTH_MAX_HIT_POINTS - Math.floor(this.healthBars[1].hitPoints), 124,
-
         );
     }
 
@@ -106,5 +128,9 @@ export class StatusBar {
     draw(context) {
         this.drawHealthBars(context);
         this.drawTime(context);
+
+        if (this.gameOver) { 
+            this.onEnd(context); // Only call onEnd if the game is over
+        }
     }
 }
